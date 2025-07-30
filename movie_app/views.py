833f3b324928a22d2036234c787df4006d196fbd
Mhdnisam <mhdnisamm@gmail.com>
2025-07-30 14:37:15 +0530

    last

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,44 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def hello(request):
+    movie_details = {'movies':  [{
+        'title': 'shidhath',
+        'year': 2017,
+        'category': 'love story',
+        'success': True,
+    },
+     {
+        'title': 'how to train your dragon',
+        'year': 2025,
+        'category': 'anime movie',
+        'success': True,
+    },
+     {
+        'title': 'pulimurugan',
+        'year': 2017,
+        'category': 'movie',
+        'success': True,
+    },
+     {
+        'title': 'bilal',
+        'year': 2013,
+        'category': 'gangster movie',
+        'success': True,
+    },
+     {
+        'title': 'ashique 2',
+        'year': 2014,
+        'category': 'love story',
+        'success': True,
+    },
+     {
+        'title': 'one piece',
+        'year': 1995,
+        'category': 'anime',
+        'success': True,
+    }]}
+    return render(request, 'hello.html', movie_details)
+
